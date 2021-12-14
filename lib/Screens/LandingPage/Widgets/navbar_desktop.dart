383a226719eb2b95f303e3c296ae0f12b134d3f0
Ahsan Iqbal <axan.iqbal1234@gmail.com>
2021-12-14 18:02:49 +0500

    .

--- lib/Screens/LandingPage/Widgets/navbar_desktop.dart
+++ lib/Screens/LandingPage/Widgets/navbar_desktop.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:nurow/Authentication/Login/login.dart';
 import 'package:nurow/Screens/LandingPage/Widgets/navbar_items.dart';
 import 'package:nurow/Screens/LandingPage/Widgets/navbar_logo.dart';
